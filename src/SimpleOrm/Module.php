605a2d034501cd2ba6bc7c9d9ace1d7bfe188e89
newage <vadim.leontiev@gmail.com>
2015-11-20 20:57:49 +0200

    [2] Add base structure

---
+++ src/SimpleOrm/Module.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SimpleOrm;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'ZendLoaderClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        $config = require __DIR__ . '/../../config/module.config.php';
+        return $config;
+    }
+}
